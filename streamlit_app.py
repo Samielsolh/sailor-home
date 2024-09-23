f63c3ae1f4ac2176ee73b2b9f7153d57a6a14b28
Sami El Solh <Samielsolh@gmail.com>
2024-09-23 22:05:00 +0000

    sailor

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import datetime
 with st.container():
     st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
 
-    # Add logo image and make it larger by adjusting the width
+    # Add logo image and x5make it larger by adjusting the width
     st.image("https://cdn.prod.website-files.com/66c72983bae166134aceff30/66c72e9088b2843fa5edb28d_Sailor%20Logo%20Green-p-500.png", width=300)
 
     # Calculate day difference and display counter
